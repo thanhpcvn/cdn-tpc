81f4d310d47209d052bb69e8404f3abc389086b2
thanhpcvn <82900245+thanhpcvn@users.noreply.github.com>
2021-09-03 03:25:49 +0700

    Create thanhpc.js

---
+++ assets/css/thanhpc.js
@@ -0,0 +1,15 @@
+ console.log(
+    "%c Website: Sso.TpcLive.Me %c",
+    'font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;font-size:24px;color:#ff00ff;-webkit-text-fill-color:#ff00ff;-webkit-text-stroke: 1px #ff00ff;',
+    "font-size:12px;color:#999999;"
+);
+console.log(
+    "%c Coder: Thành PC %c",
+    'font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;font-size:24px;color:#00ff00;-webkit-text-fill-color:#00ff00;-webkit-text-stroke: 1px #00ff00;',
+    "font-size:12px;color:#999999;"
+);
+console.log(
+    "%c Contact: https://fb.com/ThanhPCOfficialAccount/ %c",
+    'font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;font-size:24px;color:#FF0000;-webkit-text-fill-color:#FF0000;-webkit-text-stroke: 1px #FF0000;',
+    "font-size:12px;color:#999999;"
+);
